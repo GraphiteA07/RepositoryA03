99f631afa8a5461783e28f04a58a49a46e5f0d0c
Syed Zain Raza Abbas <infopit327@gmail.com>
2023-12-18 07:04:37 +0500

    Working on Responsiveness

---
+++ nav.js
@@ -0,0 +1,53 @@
+const mobileNavbarIcon=document.querySelector('.mobile-navbar-icon');
+const header=document.querySelector('.header');
+
+mobileNavbarIcon.addEventListener ('click', () => {
+	alert('Welcome')
+	// header.classList.toggle('open')
+});
+
+
+
+
+
+
+
+
+
+
+
+// const mobileNavbarBtn=document.querySelector('.mobile-navbar-icon');
+// const header=document.querySelector('.header');
+
+// const mobileNavbarBtn = () =>{
+	// alert('WoW');
+// }
+
+
+  // mobileNavbarBtn.addEventListener('click', () => mobileNavbarBtn());
+
+
+
+
+
+
+// mobileNavbarBtn.addEventListener('click', ()=>{
+	// header.classList.toggle('open')
+// })
+
+
+// const mobileNavbarBtn= document.querySelector('.mobile-navbar-btn')
+// const mobileNavbarIcon= document.querySelector('.mobile-navbar-btn i')
+
+
+// const header= document.querySelector('.header')
+
+// mobileNavbarBtn.onclick = function () {
+	// mobileNavbarBtn.classList.toggle('open')
+	// const isOpen=dropdonwMenu.classList.contains('open')
+	
+	// mobileNavbarIcon.classList=isOpen
+	// ?'fa-solid fa-xmark'
+	// :'fa-solid fa-bars'
+// }
+
